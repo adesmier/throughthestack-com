34114879e713172f6c46d80250d28aafeee74b9e
Anthony Desmier <adesmier@gmail.com>
2018-08-29 14:03:27 +0100

    Moved npm scripts to javascript folder #23

---
+++ javascript/react/index.jsx
@@ -0,0 +1,8 @@
+import React      from "react";
+import { render } from 'react-dom';
+import Blog  from './pages/blog/Blog';
+
+render(
+    <Blog />,
+    document.getElementById('blog__react-root')
+);
