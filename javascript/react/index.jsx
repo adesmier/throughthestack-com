39e9820e921ed7b0be0957c1d1b8ce408dc45486
Anthony Desmier <adesmier@gmail.com>
2018-10-31 16:55:23 +0000

    Exposed react apis to window to allow calling components from legacy code #16

--- javascript/react/index.jsx
+++ javascript/react/index.jsx
@@ -1,8 +1,18 @@
 import React      from "react";
-import { render } from 'react-dom';
-import Blog  from './pages/blog/Blog';
+import ReactDOM   from 'react-dom';
+import Blog       from './pages/blog/Blog';
 
-render(
+//expose react api globally in order to call react comps from legacy js
+window.React    = React;
+window.ReactDOM = ReactDOM;
+
+//setup react comps globally in order to be called later from legacy js
+window.searchComponent = require('./pages/search/Search').default;
+
+
+
+//TODO: check if we're on the home/blog page
+ReactDOM.render(
     <Blog />,
     document.getElementById('blog__react-root')
 );
