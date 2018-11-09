cfec41cac62fa46ae80efc94b97f465b7e27b51d
Anthony Desmier <adesmier@gmail.com>
2018-11-09 17:13:30 +0000

    WIP Now queries algolia for recent posts. Need to fix styling for length of tags which causes issue with flex grid #24

--- javascript/react/pages/Blog.jsx
+++ javascript/react/pages/Blog.jsx
@@ -3,67 +3,89 @@ import React   from 'react';
 import Article from '../components/Article/Article';
 import Tags    from '../components/Article/Tags';
 
+const AlgoliaActions = require('../../algoliasearch/modules/algoliaActions');
+
 
 export default class Blog extends React.Component {
 
-    state = {
-        posts: [
-            {
-                "objectID": 1,
-                "title": "Create a CMS for Jekyll using Contentful and Codeship",
-                "category": "tutorials",
-                "heading": "Jekyll is a powerful tool that will allow you to build a static site. But when it comes to end users adding and managing content it can become tricky due to the lack of a CMS. Contentful serves as an API driven CMS solution and when integrated with Codeship and Bitbucket you can create yourself a full website deployment pipeline.",
-                "image": "/assets/images/posts/tutorials/cms-jekyll-contentful/header.jpg",
-                "thumbnail": "/assets/images/posts/tutorials/cms-jekyll-contentful/thumbnail.jpg",
-                "date": "07 Oct 2016",
-                "time": "30 min",
-                "_tags": [
-                    "jekyll",
-                    "contentful",
-                    "codeship",
-                    "aerobatic"
-                ],
-                "url": "/blog/tutorials/create-cms-jekyll-using-contentful-codeship"
-            },
-            {
-                "objectID": 2,
-                "title": "Customise the Order of a Jekyll Post Loop",
-                "category": "devbytes",
-                "heading": "By default, Jekyll will organise your posts in date order when looping through them. However you may want, for example, your most popular posts to go first and then the rest organised by date order. In this DevByte we'll look at customising the loop order using liquid.",
-                "image": "/assets/images/posts/devbytes/devbytes-jekyll.jpg",
-                "thumbnail": "/assets/images/posts/devbytes/devbytes-jekyll-thumb.jpg",
-                "date": "02 Jan 2016",
-                "time": "5 min",
-                "_tags": [
-                    "jekyll",
-                    "blog",
-                    "liquid"
-                ],
-                "url": "/blog/devbytes/customise-order-jekyll-post-loop"
-            },
-            {
-                "objectID": 3,
-                "title": "My Windows Subsystem for Linux Development Setup",
-                "category": "devbytes",
-                "heading": "With the Windows Subsystem for Linux now becoming a full feature in Windows 10, you'll really be able to harness the power of having a full version of Linux running behind the scenes to provide compilation and tooling support for your development projects.",
-                "image": "/assets/images/posts/devbytes/devbytes-wsl.jpg",
-                "thumbnail": "/assets/images/posts/devbytes/devbytes-wsl-thumb.jpg",
-                "date": "12 Jan 2018",
-                "time": "5 min",
-                "_tags": [
-                    "windows",
-                    "wsl",
-                    "build",
-                    "tooling"
-                ],
-                "url": "/blog/devbytes/windows-subsystem-dev-setup"
-            }
-        ]
+    // state = { posts: [] }
+    state = { posts: [
+        {
+            "objectID": "15",
+            "title": "A song can make or ruin a persons day if they let it get to them",
+            "category": "devbytes",
+            "heading": "By default, Jekyll will organise your posts in date order when looping through them. However you may want, for example, your most popular posts to go first and then the rest organised by date order. In this DevByte we'll look at customising the loop order using liquid.",
+            "image": "/assets/images/posts/devbytes/devbytes-jekyll.jpg",
+            "thumbnail": "/assets/images/posts/devbytes/devbytes-jekyll-thumb.jpg",
+            "unix_date": 1478649600,
+            "friendly_date": "09 Nov 2016",
+            "time": "5 min",
+            "_tags": [
+                "hosting",
+                "css",
+                "babel",
+                "ubuntu"
+            ],
+            "url": "/blog/devbytes/a-song-can-make-or-ruin-a-persons-day-if-they-let-it-get-to-them"
+        },
+        {
+            "objectID": "17",
+            "title": "My Mum tries to be cool by saying that she likes all the same things",
+            "category": "devbytes",
+            "heading": "By default, Jekyll will organise your posts in date order when looping through them. However you may want, for example, your most popular posts to go first and then the rest organised by date order. In this DevByte we'll look at customising the loop order using liquid.",
+            "image": "/assets/images/posts/devbytes/devbytes-jekyll.jpg",
+            "thumbnail": "/assets/images/posts/devbytes/devbytes-jekyll-thumb.jpg",
+            "unix_date": 1470610800,
+            "friendly_date": "08 Aug 2016",
+            "time": "5 min",
+            "_tags": [
+                "compression",
+                "gatsby",
+                "css"
+            ],
+            "url": "/blog/devbytes/my-mum-tries-to-be-cool-by-saying-that-she-likes-all-the-same-things"
+        },
+        {
+            "objectID": "19",
+            "title": "The clock within this blog and the clock on my laptop are 1 hour",
+            "category": "devbytes",
+            "heading": "By default, Jekyll will organise your posts in date order when looping through them. However you may want, for example, your most popular posts to go first and then the rest organised by date order. In this DevByte we'll look at customising the loop order using liquid.",
+            "image": "/assets/images/posts/devbytes/devbytes-jekyll.jpg",
+            "thumbnail": "/assets/images/posts/devbytes/devbytes-jekyll-thumb.jpg",
+            "unix_date": 1466463600,
+            "friendly_date": "21 Jun 2016",
+            "time": "5 min",
+            "_tags": [
+                "babel"
+            ],
+            "url": "/blog/devbytes/the-clock-within-this-blog-and-the-clock-on-my-laptop-are-1-hour"
+        }
+    ] }
+
+
+    //--- LIFECYCLE FUNCTIONS ---
+
+    async componentDidMount() {
+        const algoliaActions = new AlgoliaActions('sort_by_date_', true);
+
+        try {
+            let recentPostResults = await algoliaActions.searchIndex('google');
+
+            console.log(recentPostResults.hits);
+
+
+            // this.setState({ posts: recentPostResults.hits });
+        } catch(e) {
+            console.error(e);
+        }
+
     }
 
 
     render() {
         const { posts } = this.state;
+
+        console.log('state', posts);
 
         const renderedPosts = posts.map(post => {
             return (
@@ -81,12 +103,17 @@ export default class Blog extends React.Component {
         });
 
         return (
-            <section
-                id="blog__blog-post-section-wrapper"
-                className="row section multi-card-flex-container"
-            >
-                {renderedPosts}
-            </section>
+            renderedPosts.length === 0 ? (
+                <div>Loading posts...</div>
+            ) : (
+                <section
+                    id="blog__blog-post-section-wrapper"
+                    className="row section multi-card-flex-container"
+                >
+                    {renderedPosts}
+                </section>
+            )
         );
+
     }
 }
