3a97473c4a18bf4b9f3225c99ee2221d7fff1022
Anthony Desmier <adesmier@gmail.com>
2018-11-14 14:06:43 +0000

    Category name is now outputted to article summary #16

--- javascript/react/components/Article/CoverImage.jsx
+++ javascript/react/components/Article/CoverImage.jsx
@@ -1,5 +1,7 @@
-import React  from 'react';
-import PropTypes from 'prop-types';
+import React              from 'react';
+import PropTypes          from 'prop-types';
+
+import CategoryIconOverlay from '../../components/Article/CategoryIconOverlay';
 
 
 const CoverImage = props => {
@@ -23,6 +25,7 @@ const CoverImage = props => {
                 }
                 style={backgroundStyle}
             >
+                <CategoryIconOverlay category={postData.category} />
             </figure>
         </a>
     );
